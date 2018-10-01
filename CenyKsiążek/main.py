c0d66ea6fe0265cf5e945c7b9297a045b9ab3c51
Armin <armin.derencz@gmail.com>
2018-10-01 14:17:13 +0200

    Dodanie warunku zakończenia działania programu.

--- CenyKsiążek/main.py
+++ CenyKsiążek/main.py
@@ -11,59 +11,74 @@ import add_book_to_database as add_to_db
 print('Witaj')
 print('###############')
 
-ok = input('Wciśnij: \n 1 aby zobaczyc listę książek \n 2 aby dodac nowe dane do bazy ')
-
-if ok == '1':
-    
-    query = '''
-            SELECT  titles, prices, date
-            FROM Titles 
-            INNER JOIN Prices
-                ON Titles.id=Prices.id
-            INNER JOIN AddDate
-                ON AddDate.id=prices.id 
-        '''
-    df = pd.DataFrame(pd.read_sql(query,create_database.engine) )
-    df.index = df.index + 1
-
-    print (df)
-    
-elif ok == '2':
-        
-        
-    
-        
-        add_to_db.get_date()
-        
-        add_to_db.login_to_Account()
-    
-        
-        add_to_db.get_data_from_webpage()
-    
-    
-id = input('Wybierz id książki: ')
+while id != 't':  
+    print(('Wciśnij: \n 1 aby zobaczyc dostepne tytuly \n 2 aby dodac nowe dane do bazy \n 3 aby zobaczyc wszystkie dane '))
+    ok = input('')
+    print("------------------------------------------------------")
 
 
-query = '''
-            SELECT  titles, prices, date
-            FROM Titles 
-            INNER JOIN Prices
-                ON Titles.id=Prices.id
-            INNER JOIN AddDate
-                ON AddDate.id=prices.id 
-            WHERE Titles.titles = (SELECT titles FROM Titles WHERE Titles.id= '''+id+''')
+    if ok == '1':
+    
+
+        query = '''
+                SELECT  DISTINCT titles
+                FROM Titles 
+                '''
+        df = pd.DataFrame(pd.read_sql(query,create_database.engine) )
+        df.index = df.index + 1
+        df.columns = ['nazwa']
+        
+        print('                DOSTĘPNE KSIĄŻKI')
+        print(df)
             
-            AND (Titles.id !=''' +id + '''
-            OR Titles.id =''' +id+''')'''
+    elif ok == '2':
+    
+        add_to_db.get_date()  
+        add_to_db.login_to_Account()   
+        add_to_db.get_data_from_webpage()
         
-df2 = pd.DataFrame(pd.read_sql(query,create_database.engine) )
-df2.index = df2.index + 1
-print (df2)
+    elif ok == '3':
+    
+        query = '''
+                SELECT  titles, prices, date
+                FROM Titles 
+                INNER JOIN Prices
+                    ON Titles.id=Prices.id
+                INNER JOIN AddDate
+                    ON AddDate.id=prices.id 
+            '''
+        df = pd.DataFrame(pd.read_sql(query,create_database.engine) )
+        df.index = df.index + 1
 
-x_col = df2['date']
-y_col= df2['prices']
+        print (df)
+    
+    
+  
+    id = input('Wybierz numer książki: ')
 
-first_title = df2.loc[1,'titles']
-str(first_title)
 
-plot.createPlot(x_col, y_col, first_title)
+    query = '''
+                SELECT  titles, prices, date
+                    FROM Titles 
+                INNER JOIN Prices
+                    ON Titles.id=Prices.id
+                INNER JOIN AddDate
+                    ON AddDate.id=prices.id 
+                WHERE Titles.titles = (SELECT titles FROM Titles WHERE Titles.id= '''+id+''')
+            
+                AND (Titles.id !=''' +id + '''
+                OR Titles.id =''' +id+''')'''
+        
+    df2 = pd.DataFrame(pd.read_sql(query,create_database.engine) )
+    df2.index = df2.index + 1
+    print (df2)
+
+    x_col = df2['date']
+    y_col= df2['prices']
+
+    first_title = df2.loc[1,'titles']
+
+    plot.createPlot(x_col, y_col, first_title)
+    id = input('czy zakonczyc? t/n ')
+    
+print("Koniec")
